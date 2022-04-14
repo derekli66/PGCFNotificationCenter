afd6323e70482391a0f4cdb1714585a3a65db495
Derek Lee <derek.lee@positivegrid.com>
2022-04-14 13:32:42 +0800

    Initial Commit

---
+++ PGCFNotificationCenter/PGCFNotificationCenter.h
@@ -0,0 +1,18 @@
+//
+//  PGCFNotificationCenter.h
+//  PGCFNotificationCenter
+//
+//  Created by CHIEN-MING LEE on 2022/4/14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PGCFNotificationCenter.
+FOUNDATION_EXPORT double PGCFNotificationCenterVersionNumber;
+
+//! Project version string for PGCFNotificationCenter.
+FOUNDATION_EXPORT const unsigned char PGCFNotificationCenterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PGCFNotificationCenter/PublicHeader.h>
+
+
